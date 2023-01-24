79f2f0285febcd5686e88d38eebdde5244650b8b
Vinayak-2108 <vinayakchatterjee28@gmail.com>
2023-01-24 23:33:03 +0530

    debugging gif issue

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -11,11 +11,12 @@ const useFetch = ({ keyword }) => {
           .split(" ")
           .join("")}&limit=1`
       );
+      console.log("Yes");
       const { data } = await response.json();
       setGifUrl(data[0]?.images?.downsized_medium?.url);
     } catch (error) {
       setGifUrl(
-        '"https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"'
+        'https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284'
       );
     }
   };
